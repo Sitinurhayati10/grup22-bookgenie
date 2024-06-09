eafcac016ca6e3fee88d460dd4497d9f282aba2b
Sitinurhayati10 <90059192+Sitinurhayati10@users.noreply.github.com>
2024-06-09 11:46:31 +0700

    Update main-checkpoint.py

--- main-checkpoint.py
+++ main-checkpoint.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
+from googledriver import download
 from sklearn.svm import SVC
 import pandas as pd
 from io import BytesIO
 from PIL import Image
 import base64
+import zipfile
 
 # Fungsi untuk memuat gambar sebagai base64
 def load_image_as_base64(image_path):
@@ -92,19 +94,26 @@ if st.button("Prediksi"):
         st.warning("Mohon isi deskripsi buku terlebih dahulu.")
     else:
         st.info("Sedang melakukan prediksi...")
-
+       
+        #unzip model svm
+        with zipfile.ZipFile('svm_model.zip', 'r') as zip_ref:
+             zip_ref.extractall()
+             
         # Load model SVM dan vectorizer
-        with open("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\svm_model.pkl", 'rb') as file:
+        with open("./svm_model.pkl", 'rb') as file:
             loaded_model = pickle.load(file)
-        
-        with open("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\tfidf_vectorizer.pkl", 'rb') as file:
+            
+            
+        with zipfile.ZipFile('tfidf_vectorizer.zip', 'r') as zip_ref:
+             zip_ref.extractall()
+        with open("./tfidf_vectorizer.pkl", 'rb') as file:
             tfidf = pickle.load(file)
         
         # Preprocessing deskripsi buku
         book_description_processed = [stem_text(remove_sw(removepunc(lowercase(book_description))))]
 
         # Membaca data X_train
-        X_train = pd.read_csv("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\X_train_tfidf.csv")  # Ubah sesuai dengan lokasi yang benar
+        X_train = pd.read_csv("./X_train_tfidf.csv")  # Ubah sesuai dengan lokasi yang benar
         
         # Menerapkan pemrosesan teks pada data X_train
         X_train['Combined_Text'] = X_train['Combined_Text'].apply(lowercase)
